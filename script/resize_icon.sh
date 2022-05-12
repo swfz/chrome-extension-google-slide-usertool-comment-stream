1b0d857038b6300bd81e0b69c419eef3219aa6b8
swfz <sawafuji.09@gmail.com>
2022-05-13 01:29:39 +0900

    chore: convert script

---
+++ script/resize_icon.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+convert -resize 16x16 images/128x128.png images/16x16.png
+convert -resize 32x32 images/128x128.png images/32x32.png
+convert -resize 48x48 images/128x128.png images/48x48.png
