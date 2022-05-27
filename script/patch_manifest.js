bb51a7e15069a5ee24afa6bc20014f347061235d
swfz <sawafuji.09@gmail.com>
2022-05-28 04:37:25 +0900

    chore: #2 patch script

---
+++ script/patch_manifest.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const manifest = JSON.parse(fs.readFileSync('dist/manifest.json'));
+
+const resources = manifest.web_accessible_resources.map(item => item.resources).flat();
+console.log(resources);
+
+const patchedManifest = {
+  ...manifest,
+  web_accessible_resources: [
+    {
+      matches: [
+        "<all_urls>"
+      ],
+      resources: [...resources, "images/sign_language_black_24dp.svg"],
+      use_dynamic_url: true
+    }
+  ]
+}
+
+fs.writeFileSync('dist/manifest.json',JSON.stringify(patchedManifest, null, 2));
