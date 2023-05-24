6cd22c13e206e971855ac6af1c319a5b98e486e2
swfz <sawafuji.09@gmail.com>
2023-05-24 09:50:35 +0900

    chore: format

--- script/patch_manifest.js
+++ script/patch_manifest.js
@@ -2,20 +2,18 @@ const fs = require('fs');
 
 const manifest = JSON.parse(fs.readFileSync('dist/manifest.json'));
 
-const resources = manifest.web_accessible_resources.map(item => item.resources).flat();
+const resources = manifest.web_accessible_resources.map((item) => item.resources).flat();
 console.log(resources);
 
 const patchedManifest = {
   ...manifest,
   web_accessible_resources: [
     {
-      matches: [
-        "<all_urls>"
-      ],
-      resources: [...resources, "images/sign_language_black_24dp.svg"],
-      use_dynamic_url: true
-    }
-  ]
-}
+      matches: ['<all_urls>'],
+      resources: [...resources, 'images/sign_language_black_24dp.svg'],
+      use_dynamic_url: true,
+    },
+  ],
+};
 
-fs.writeFileSync('dist/manifest.json',JSON.stringify(patchedManifest, null, 2));
+fs.writeFileSync('dist/manifest.json', JSON.stringify(patchedManifest, null, 2));
