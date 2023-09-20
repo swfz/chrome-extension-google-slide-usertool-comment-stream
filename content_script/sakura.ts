c0381b9d0737643f5a9fe72a06da929a93549659
swfz <sawafuji.09@gmail.com>
2023-09-21 00:12:06 +0900

    feat: 聴講者と同じ画面のcontent_scriptを追加

---
+++ content_script/sakura.ts
@@ -0,0 +1,35 @@
+console.log('loaded google slide comment stream');
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log('subscribe plant comment when page changed started');
+
+  const broadcastChannel = new BroadcastChannel('plant_comment_channel');
+
+  const handleEvent = (event) => {
+    const textarea = document.querySelector<HTMLTextAreaElement>('.punch-ask-question-submit-question-dialog-question-textarea');
+    if (textarea === null) {
+      return
+    }
+    textarea.click();
+
+    const anonimity = document.querySelector<HTMLInputElement>('.docs-material-gm-labeled-checkbox-checkbox');
+    if (anonimity?.ariaChecked === 'false') {
+      anonimity.click();
+    }
+
+    const inputEvent = new Event('input', { 'bubbles': true, 'cancelable': true });
+    textarea.value = event.data;
+    textarea.dispatchEvent(inputEvent);
+
+    const button = document.querySelector('.punch-ask-question-submit-button')
+
+    const mousedownEvent = new MouseEvent("mousedown", { bubbles: true, cancelable: true, view: window, button: 0 });
+    button?.dispatchEvent(mousedownEvent);
+
+    const mouseupEvent = new MouseEvent("mouseup", { bubbles: true, cancelable: true, view: window, button: 0 });
+    button?.dispatchEvent(mouseupEvent);
+  };
+
+  broadcastChannel.onmessage = handleEvent;
+  sendResponse({ screenType: 'usertool', message: 'A listener has been added to the usertool.' });
+});
